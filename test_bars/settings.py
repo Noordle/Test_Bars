daeeba0760793e7c77f6fd114aa689ecede45e33
Noordle <noordle1537@gmail.com>
2017-12-03 18:11:36 +0300

    Heroku

--- test_bars/settings.py
+++ test_bars/settings.py
@@ -1,3 +1,15 @@
 from .base_settings import *
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'test',
+        'USER': 'artem',
+        'PASSWORD': 'qwerty322',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
+    }
+}
+EMAIL_HOST_USER = 'noordle1537@gmail.com'
 
+EMAIL_HOST_PASSWORD = 'c4avfyr8bvs77zc0'
 
